8d143d7523cb5c582d793cca621433564d8962cf
kourosh <kourosh23@hotmail.com>
2025-07-19 22:13:32 +0200

    product get

--- App10.Api/Controllers/ProductsController.cs
+++ App10.Api/Controllers/ProductsController.cs
@@ -27,5 +27,28 @@ namespace App10.Api.Controllers
             _apiResponse.StatusCode = HttpStatusCode.OK;
             return Ok(_apiResponse);
         }
+
+        [HttpGet("{id:int}", Name ="GetProduct")]
+        public IActionResult Get(int id)
+        {
+            if (id <= 0)
+            {
+                _apiResponse.IsSuccessful = false;
+                _apiResponse.Errors.Add("Invalid ID.");
+                _apiResponse.StatusCode = HttpStatusCode.BadRequest;
+                return BadRequest(_apiResponse);
+            }
+            var product = _db.Products.FirstOrDefault(p => p.Id == id);
+            if (product == null)
+            {
+                _apiResponse.IsSuccessful = false;
+                _apiResponse.Errors.Add("Item not found.");
+                _apiResponse.StatusCode = HttpStatusCode.NotFound;
+                return NotFound(_apiResponse);
+            }
+            _apiResponse.Result = product;
+            _apiResponse.StatusCode = HttpStatusCode.OK;
+            return Ok(_apiResponse);
+        }
     }
 }
