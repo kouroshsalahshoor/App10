d5041d2645e2927f5ee45e92043f2e9e0a9d0ad0
kourosh <kourosh23@hotmail.com>
2025-07-20 08:46:54 +0200

    product create update dtos

---
+++ App10.Api/Dtos/ProductUpdateDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace App10.Api.Dtos
+{
+    public class ProductUpdateDto
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        public string Category { get; set; } = string.Empty;
+        public string Tag { get; set; } = string.Empty;
+        [Range(0.01, 10000.00)]
+        public double Price { get; set; }
+        public string? Description { get; set; }
+        public IFormFile File { get; set; } = null!; // Use null-forgiving operator to indicate that this will be set later
+    }
+}
