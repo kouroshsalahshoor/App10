86eb97dd77c1283e6f13870ea4127769100d62b6
kourosh <kourosh23@hotmail.com>
2025-07-20 08:46:54 +0200

    product create update dtos

--- App10.Api/Dtos/ProductCreateDto.cs
+++ App10.Api/Dtos/ProductCreateDto.cs
@@ -7,7 +7,7 @@ namespace App10.Api.Dtos
         [Required]
         public string Name { get; set; } = string.Empty;
         public string Category { get; set; } = string.Empty;
-        public string Tag { get; set; } = string.Empty;
+        public string? Tag { get; set; }
         [Range(0.01, 10000.00)]
         public double Price { get; set; }
         public string? Description { get; set; }
